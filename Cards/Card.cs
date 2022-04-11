2516dd4dcce25a65040e3ae53be2a71aab066968
Repcaknalim <97058400+Repcaknalim@users.noreply.github.com>
2022-04-11 12:07:52 +0200

    Add files via upload

---
+++ Cards/Card.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cards
+{
+    public class Paper
+    {
+        public static string type = "white";
+
+        public static void Print(List<Paper> papers)
+        {
+            foreach (Paper p in papers)
+            {
+                Console.WriteLine($"Paper: {type}");
+            }
+        }
+    }
+
+    public class Card : Paper, IComparable<Card>
+    {
+        public Suit Suit { get; private set; }
+        public Value Value { get; private set; }
+        public string Name {
+            get
+            {
+                return $"{Value} of {Suit}";
+            }
+            private set { } 
+        }
+
+        public Card (Value value, Suit suit)
+        {
+            Suit = suit;
+            Value = value;
+        }
+
+        public int CompareTo(Card cardToCompare)
+        {
+            if (Value > cardToCompare.Value)
+            {
+                return 1;
+            }
+            else if (Value < cardToCompare.Value)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+    class CardCompareBySuit : IComparer<Card>
+    {
+        public int Compare(Card x, Card y)
+        {
+            if (x.Suit > y.Suit)
+            {
+                return -1;
+            }
+            if (x.Suit < y.Suit)
+            {
+                return 1;
+            }
+            if (x.Value > y.Value)
+            {
+                return -1;
+            }
+            if (x.Value < y.Value)
+            {
+                return 1;
+            }
+            return 0;
+        }
+    }
+}
---
+++ Cards/CardDeck.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cards
+{
+    public class CardDeck
+    {
+        static Random random = new Random();
+
+        private readonly List<Card> cards = new List<Card>()
+        {
+            new Card(Value.Ace, Suit.spades),
+            new Card((Value)random.Next(1, 14), (Suit)random.Next(4)),
+            new Card((Value)random.Next(1, 14), (Suit)random.Next(4)),
+            new Card((Value)random.Next(1, 14), (Suit)random.Next(4)),
+        };
+
+        public void PrintDeck()
+        {
+            if (cards.Count > 0)
+            {
+                Console.WriteLine("Deck: ");
+                int i = 0;
+                foreach (var card in cards)
+                {
+                    Console.WriteLine($"Card nr {++i}: {card}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Deck is empty.");
+            }
+
+            IEnumerable<Paper> paperCards = cards;
+            Paper.Print(paperCards.ToList());
+        }
+
+        public void AddCard()
+        {
+            Console.WriteLine("dd a card: ");
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine($"Press {i} to add {(Suit)i}.");
+            }
+            Console.WriteLine("Pick a colour: ");
+
+            if (int.TryParse(Console.ReadKey().KeyChar.ToString(), out int suit))
+            {
+                for (int i = 1; i < 14; i++)
+                {
+                    Console.WriteLine($"Write {i} to add {(Value)i}.");
+                }
+                Console.WriteLine("Pick a value: ");
+                if (int.TryParse(Console.ReadLine(), out int value))
+                {
+                    Card card = new Card((Value)value, (Suit)suit);
+                    cards.Add(card);
+                }
+            }
+            cards.Sort();
+        }
+
+        public void RemoveCard()
+        {
+            Console.WriteLine("elete a card (pick number): ");
+            if (int.TryParse(Console.ReadLine(), out int cardNumber) && cardNumber >= 1 && cardNumber <= cards.Count)
+            {
+                Console.WriteLine($"Deleteing: {cards[cardNumber - 1].Name}");
+                cards.RemoveAt(cardNumber - 1);
+            }
+
+            IComparer<Card> suitComparer = new CardCompareBySuit();
+            cards.Sort(suitComparer);
+        }
+    }
+}
