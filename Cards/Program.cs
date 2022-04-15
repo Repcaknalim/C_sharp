e3fd1405ca104f46baa1fe58baf6416db125e045
Repcaknalim <97058400+Repcaknalim@users.noreply.github.com>
2022-04-15 09:40:08 +0200

    Add files via upload

---
+++ Cards/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Cards;
+
+CardDeck cardDeck = new CardDeck();
+
+while(true)
+{
+    cardDeck.PrintDeck();
+    Console.WriteLine("Press 'a' to add or 'd' to delete a card");
+    char key = Console.ReadKey().KeyChar;
+
+    switch(key)
+    {
+        case 'a':
+        case 'A':
+            cardDeck.AddCard();
+            break;
+        case 'd':
+        case 'D':
+            cardDeck.RemoveCard();
+            break;
+        default:
+            return;
+    }
+}
