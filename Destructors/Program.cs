f65c373656e96ef8d6cc4227280d50bd8b1df843
Repcaknalim <97058400+Repcaknalim@users.noreply.github.com>
2022-04-15 12:31:33 +0200

    Add files via upload

---
+++ Destructors/Program.cs
@@ -0,0 +1,40 @@
+﻿
+namespace Destructors
+{
+    class Program
+    {
+        public class Objectt
+        {
+            public static int ObjCount = 0;
+            public int ObjID { get; } = ++ObjCount;
+            public Objectt() => Console.WriteLine("Object nr.{0}.", ObjID);
+            ~Objectt()
+            {
+                Console.WriteLine($"Object nr.{ObjID} has been destroyed");
+            }
+        }
+
+        static void Main()
+        {
+            var Stopwatch = System.Diagnostics.Stopwatch.StartNew();
+            var objects = new List<Objectt>();
+            while(true)
+            {
+                switch (Console.ReadKey(true).KeyChar)
+                {
+                    case 'a':
+                        objects.Add(new Objectt());
+                        break;
+                    case 'c':
+                        Console.WriteLine("Empting the list... Time: {0}", Stopwatch.ElapsedMilliseconds);
+                        objects.Clear();
+                        break;
+                    case 'g':
+                        Console.WriteLine("Reclaiming memory... Time: {0}", Stopwatch.ElapsedMilliseconds);
+                        GC.Collect();
+                        break;
+                };
+            }
+        }
+    }
+}
