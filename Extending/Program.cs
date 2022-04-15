65f7be856a54e0b11455f397ad9ef5ee772bcbe2
Repcaknalim <97058400+Repcaknalim@users.noreply.github.com>
2022-04-15 12:39:09 +0200

    Add files via upload

---
+++ Extending/Program.cs
@@ -0,0 +1,24 @@
+﻿using Extensions;
+
+namespace Extending
+{
+    class Program
+    {
+        static void Main()
+        {
+            string message = "Kot! Help!";
+            Console.WriteLine(message.IsDistressCall());
+        }
+    }
+}
+
+namespace Extensions
+{
+    public static class Extensions
+    {
+        public static bool IsDistressCall(this string s)
+        {
+            return s.Contains("Help!");
+        }
+    }
+}
