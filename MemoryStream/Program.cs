922ccc0573655d25b1846be19ea5b84d4d764a83
Repcaknalim <97058400+Repcaknalim@users.noreply.github.com>
2022-04-15 09:38:28 +0200

    Add files via upload

---
+++ MemoryStream/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        using (var ms = new MemoryStream())
+        {
+            using (var sw = new StreamWriter(ms))
+            {
+                sw.WriteLine("Value: {0:00.0}.", 123.456789);
+            }
+            Console.WriteLine(Encoding.UTF8.GetString(ms.ToArray()));
+        }
+    }
+}
