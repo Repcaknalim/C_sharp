9a709a27fc3f6149604e93b69dd6bdcfb5a72a60
Repcaknalim <97058400+Repcaknalim@users.noreply.github.com>
2022-04-15 09:38:02 +0200

    Add files via upload

---
+++ StreamReader/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        var folder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+        var reader = new StreamReader($"{folder}{Path.DirectorySeparatorChar}thePlan.txt");
+        var writer = new StreamWriter($"{folder}{Path.DirectorySeparatorChar}email.txt");
+
+        writer.WriteLine("From: Kot");
+        writer.WriteLine("To: Dog");
+
+        while(!reader.EndOfStream)
+        {
+            var line = reader.ReadLine();
+            writer.WriteLine($"Kot -> {line}");
+        }
+
+        writer.WriteLine();
+        writer.WriteLine("MEOW");
+
+        writer.Close();
+        reader.Close();
+    }
+}
