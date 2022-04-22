045ee52d8a7a3ce08908e55557430428734282f4
Repcaknalim <97058400+Repcaknalim@users.noreply.github.com>
2022-04-22 11:13:11 +0200

    Add files via upload

---
+++ EventChain/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace EventChain
+{
+    class Program
+    {
+        static int count;
+
+        static void SaySomething(object sender, TalkEventArgs e)
+            => Console.WriteLine($"Call #{count++}: I said something: {e.Message}");
+
+        static void SaySomethingElse (object sender, TalkEventArgs e)
+            => Console.WriteLine($"Call #{count++}: I said something else: {e.Message}");
+
+        static void Main()
+        {
+            var MyEvent = new Talker();
+            while(true)
+            {
+                Console.Write("1 to chain SaySomething, 2 to chain SaySomethingElse, or a message: ");
+                var line = Console.ReadLine();
+                switch(line)
+                {
+                    case "1":
+                        Console.WriteLine("Adding SaySomething");
+                        MyEvent.TalkToMe += SaySomething;
+                        break;
+                    case "2":
+                        Console.WriteLine("Adding SaySomethingElse");
+                        MyEvent.TalkToMe += SaySomethingElse;
+                        break;
+                    default:
+                        count = 1;
+                        Console.WriteLine("Raising the TalkToMe event");
+                        MyEvent.OnTalkToMe(line);
+                        break;
+                }
+            }
+        }
+    }
+
+    class TalkEventArgs : EventArgs
+    {
+        public string Message { get; private set; }
+
+        public TalkEventArgs(string message) => Message = message;
+    }
+
+    class Talker
+    {
+        // Create the event
+        public event EventHandler<TalkEventArgs> TalkToMe;
+
+        // Invoke the event
+        public void OnTalkToMe(string message) => TalkToMe?.Invoke(this, new TalkEventArgs(message));
+    }
+}
